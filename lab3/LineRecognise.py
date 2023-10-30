9f867b99539d0807c43be1921de41ed878d5f4f2
JakubBacik <jakub99.b@gmail.com>
2023-10-30 12:18:06 +0100

    Code refactoring

--- lab3/LineRecognise.py
+++ lab3/LineRecognise.py
@@ -2,91 +2,109 @@ import json
 from scipy import optimize
 from matplotlib import collections  as mc
 import pylab as pl
-##reading data from the file
-json_data = open('line_detection_1.json')
-data = json.load(json_data)
-
-### plot in polar system
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-x = np.arange(0,512)
-theta = (np.pi/512 )*(x-256)  # angle in radians
-
-scan_data=data[0]["scan"]
-robot_position=data[0]["pose"]
-epsilon = 0.15
-x = [] 
-y = []   
-
-for i in range(0, 512):
-    x.append(scan_data[i] * math.cos(-theta[i]+robot_position[2]) + robot_position[0])
-    y.append(scan_data[i] * math.sin(-theta[i]+robot_position[2]) + robot_position[1])
-
-
-plt.figure(figsize = (10,8))
-plt.plot(x, y, 'b.')
-plt.xlabel('x')
-plt.ylabel('y')
-
-
+#Function for optimaze
 def func(x, a, b):
     y = a*x + b
     return y
 
-def curve_fitting(x, y, start):
+def open_json(name):
+    json_data = open(name)
+    data = json.load(json_data)
+
+    x = np.arange(0,512)
+    theta = (np.pi/512 )*(x-256)  
+
+    scan_data=data[0]["scan"]
+    robot_position=data[0]["pose"]
+   
+    x = [] 
+    y = []   
+
+    for i in range(0, 512):
+        x.append(scan_data[i] * math.cos(-theta[i]+robot_position[2]) + robot_position[0])
+        y.append(scan_data[i] * math.sin(-theta[i]+robot_position[2]) + robot_position[1])
+    
+    return x, y, scan_data
+
+def plot_raw_data(x, y):
+    plt.figure(figsize = (10,8))
+    plt.plot(x, y, 'b.')
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+def plot_from_line(line_tab):
+    lc = mc.LineCollection(line_tab, linewidths=2)
+
+    fig, ax = pl.subplots()
+    ax.add_collection(lc)
+    ax.autoscale()
+    ax.margins(0.1)
+
+def show_plot():
+    plt.show()
+
+def curve_fitting(x, y, start, scan_data):
     if start+2 >= len(x):
         return len(x)-1
     
     alpha_o = [0,0]
     tmp = 0
     for i in range(start+2, len(x)):
-        if scan_data[i] < 50:
+        if scan_data[i] < 100:
             tmp= tmp+1
             alpha = optimize.curve_fit(func, xdata = x[start:i], ydata = y[start:i])[0]
+            #alpha = np.polyfit(x[start:i], y[start:i], 1)
 
             if i == start+2:
+                print("first int")
                 alpha_o = alpha
             
-            if abs(alpha[0]-alpha_o[0]) > epsilon and abs(alpha[1]-alpha_o[1]) > epsilon:
-                print("Line break")
+            if abs(alpha[0]-alpha_o[0]) > epsilonA and abs(alpha[1]-alpha_o[1]) > epsilonB:
+                print("Line break", i-start)
                 return i
-            if tmp%5==0:
+            
+            if tmp%7==0:
+                print("second value")
                 alpha_o = alpha
 
         else:
-            print("NaNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNn")
+            print("=========== NaN ===========")
             return i
     
     return i
 
-idx = 0
-toPlot = []
-while idx < len(x)-1:
-    tmp = []
-    while scan_data[idx] > 50:
-        idx += 1
-    tmp.append((x[idx],y[idx]))
-    idx = curve_fitting(x,y,idx)
-    tmp.append((x[idx-1], y[idx-1]))
 
-    toPlot.append(tmp)
+def line_detection_alg(x,y, scan_data):
+    idx = 0
+    toPlot = []
+
+    while idx < len(x)-1:
+        tmp = []
+
+        while scan_data[idx] > 100:
+            idx += 1
+
+        tmp.append((x[idx],y[idx]))
+        idx = curve_fitting(x,y,idx, scan_data)
+        tmp.append((x[idx-1], y[idx-1]))
+
+        toPlot.append(tmp)
+
+    return toPlot
 
 
+for i in [0.10]:
+    epsilonA = i
+    epsilonB = i
 
-# plt.figure(figsize = (10,8))
-# plt.plot(x, y, 'b.')
-# plt.plot(x, alpha[0]*np.array(x) + alpha[1], 'r')
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.show()
+    x, y, scan_data = open_json('line_detection_1.json')
+    lines = line_detection_alg(x,y, scan_data)
 
-lc = mc.LineCollection(toPlot, linewidths=2)
+    plot_from_line(lines)
 
-fig, ax = pl.subplots()
-ax.add_collection(lc)
-ax.autoscale()
-ax.margins(0.1)
 
-pl.show()
+show_plot()
