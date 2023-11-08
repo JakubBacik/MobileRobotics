5a799bd35fe8365f359c8eb648422066da92f032
WJarzyna <252949@student.pwr.edu.pl>
2023-11-08 08:29:46 +0100

    Multipoint increment added, general cleanup

--- lab3/LineRecognise.py
+++ lab3/LineRecognise.py
@@ -2,21 +2,87 @@ import json
 from scipy import optimize
 from matplotlib import collections  as mc
 import pylab as pl
-##reading data from the file
-json_data = open('line_detection_1.json')
-data = json.load(json_data)
-
-### plot in polar system
-import matplotlib.pyplot as plt
-import numpy as np
+import argparse
 import math
 
-x = np.arange(0,512)
-theta = (np.pi/512 )*(x-256)  # angle in radians
+
+
+epsilon = 0.15
+multistep = 5
+
+
+
+def func(x, a, b):
+    y = a*x + b
+    return y
+
+
+
+
+
+def line_fitting(x, y, start):
+    if start+2 >= len(x):
+        return len(x)-1
+    
+    alpha_o = [0,0]
+    first_iter = True
+    multipoint = True
+#    tmp = 0
+
+    for i in range(start+2, len(x)):
+        if scan_data[i] > 50:
+            #print("NaNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNn")
+            return i
+        
+        else:
+            if multipoint:
+                multi_i = i
+                for j in range( i, min(len(x), i+multistep) ):
+                    if scan_data[i] > 50:
+                        i = j-1
+                        break
+
+#            tmp = tmp+1
+            alpha = optimize.curve_fit(func, xdata = x[start:i], ydata = y[start:i])[0]
+
+            if first_iter:
+                alpha_o = alpha
+                first_iter = False
+            
+            if abs(alpha[0]-alpha_o[0]) > epsilon or abs(alpha[1]-alpha_o[1]) > epsilon:
+                if multipoint:
+                    i = multi_i
+                    multipoint = False
+                else:
+                    #print("Line break")
+                    return i
+#            else:
+#                if not multipoint:
+#                    multipoint = True
+
+#            if tmp%5==0:
+#                alpha_o = alpha
+    
+    return i
+
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="name of the json data file")
+args = parser.parse_args()
+
+##reading data from file
+json_data = open(args.filename)
+data = json.load(json_data)
+
+x = pl.arange(0,512)
+theta = (pl.pi/512 )*(x-256)  # angle in radians
 
 scan_data=data[0]["scan"]
 robot_position=data[0]["pose"]
-epsilon = 0.15
+
 x = [] 
 y = []   
 
@@ -25,68 +91,38 @@ for i in range(0, 512):
     y.append(scan_data[i] * math.sin(-theta[i]+robot_position[2]) + robot_position[1])
 
 
-plt.figure(figsize = (10,8))
-plt.plot(x, y, 'b.')
-plt.xlabel('x')
-plt.ylabel('y')
-
-
-def func(x, a, b):
-    y = a*x + b
-    return y
-
-def curve_fitting(x, y, start):
-    if start+2 >= len(x):
-        return len(x)-1
-    
-    alpha_o = [0,0]
-    tmp = 0
-    for i in range(start+2, len(x)):
-        if scan_data[i] < 50:
-            tmp= tmp+1
-            alpha = optimize.curve_fit(func, xdata = x[start:i], ydata = y[start:i])[0]
-
-            if i == start+2:
-                alpha_o = alpha
-            
-            if abs(alpha[0]-alpha_o[0]) > epsilon and abs(alpha[1]-alpha_o[1]) > epsilon:
-                print("Line break")
-                return i
-            if tmp%5==0:
-                alpha_o = alpha
-
-        else:
-            print("NaNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNn")
-            return i
-    
-    return i
-
 idx = 0
-toPlot = []
+lines = []
 while idx < len(x)-1:
-    tmp = []
+    line_tmp = []
     while scan_data[idx] > 50:
-        idx += 1
-    tmp.append((x[idx],y[idx]))
-    idx = curve_fitting(x,y,idx)
-    tmp.append((x[idx-1], y[idx-1]))
+        if idx < len(x)-1:
+            idx += 1
+        else:
+            break
 
-    toPlot.append(tmp)
+    line_tmp.append( (x[idx],y[idx]) )
+    idx = line_fitting(x,y,idx)
+    line_tmp.append( (x[idx-1], y[idx-1]) )
+
+    lines.append(line_tmp)
 
 
 
-# plt.figure(figsize = (10,8))
-# plt.plot(x, y, 'b.')
-# plt.plot(x, alpha[0]*np.array(x) + alpha[1], 'r')
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.show()
-
-lc = mc.LineCollection(toPlot, linewidths=2)
-
+#Detected lines plot
+lc = mc.LineCollection( lines, linewidths=2)
 fig, ax = pl.subplots()
+
+#pl.style.use('dark_background')
+pl.get_current_fig_manager().full_screen_toggle()
+
 ax.add_collection(lc)
 ax.autoscale()
 ax.margins(0.1)
+
+#Data points plot
+#pl.plot(x, y, 'r.', markersize = 1)
+pl.xlabel('x')
+pl.ylabel('y')
 
 pl.show()
